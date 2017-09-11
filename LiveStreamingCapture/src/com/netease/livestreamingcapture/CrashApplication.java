dd5ff1144d45a55dbf9fba1b453a11865be2138e
李锐 <ray007great@foxmail,com>
2017-09-11 10:42:13 +0800

    FU Auto Commiter 55d1585

---
+++ LiveStreamingCapture/src/com/netease/livestreamingcapture/CrashApplication.java
@@ -0,0 +1,16 @@
+package com.netease.livestreamingcapture;
+
+
+import android.app.Application;
+
+public class CrashApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        CrashHandler crashHandler = CrashHandler.getInstance();
+//        crashHandler.init(getApplicationContext());
+    }
+    
+}
+
